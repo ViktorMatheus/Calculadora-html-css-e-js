bbcacb197d247364df282f259b4827096fa5823d
Victor Matheus de Sousa Santos <victor-vieira28@hotmail.com>
2021-06-01 21:15:12 -0300

    Atualização no js
    adicionado as  variaveis para os numeros da calculadora

---
+++ calculadora/function.js
@@ -0,0 +1,32 @@
+var textoResposta = document.querySelector("#resultado-da-calculadora")
+
+var numeroUm = document.querySelector("#um")
+var numeroDois = document.querySelector("#dois")
+var numeroTres = document.querySelector("#tres")
+var numeroQuatro = document.querySelector("#quatro")
+var numeroCinco = document.querySelector("#cinco")
+var numeroSeis = document.querySelector("#seis")
+var numeroSete = document.querySelector("#sete")
+var numeroOito = document.querySelector("#oito")
+var numeroNove = document.querySelector("#nove")
+var numeroZero = document.querySelector("#zero")
+
+
+numeroZero = 0
+numeroUm = 1
+numeroDois = 2
+numeroTres = 3 
+numeroQuatro = 4
+numeroCinco = 5
+numeroSeis = 6
+numeroSete = 7
+numeroOito = 8
+numeroNove = 9 
+
+Numeros = [numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,numeroNove]
+
+console.log(Numeros)
+
+function recuperarValor(){
+    
+}
